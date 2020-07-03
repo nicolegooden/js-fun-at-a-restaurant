a86eeebc50ce423048be2cf2ed08bba7b6629287
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-07-03 09:18:45 -0600

    Initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
+}
 
+function createMenuItem(name, price, type) {
+  var myItem = {
+  name: name,
+  price: price,
+  type: type
+};
+  return myItem;
+}
+
+function addIngredients(newElement, ingredients) {
+  if (!ingredients.includes(newElement)) {
+    return ingredients.push(newElement);
+  }
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`;
+}
+
+function decreasePrice(initialPrice) {
+  var discount = (initialPrice * .10);
+  var newPrice = (initialPrice - discount);
+  return newPrice;
+}
+
+function createRecipe(menuItemName, ingredients, menuItemType) {
+  var recipe = {
+    title: menuItemName,
+    ingredients: ingredients,
+    type: menuItemType
+  };
+  return recipe;
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
