b7b5fd7f46d3452c15a1de537b12d436ad7d50c1
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-07-05 11:08:20 -0600

    Initial commit

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,27 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant, morning) {
+    this.name = name,
+    this.restaurant = restaurant,
+    this.morning = morning
+  }
+
+  greetCustomer(person, morning) {
+    if (morning) {
+      return `Good morning, ${person}!`;
+    } else {
+    return `Hello, ${person}!`;
   }
 }
+
+  checkForFood(foodItem) {
+    if (this.restaurant.menus.breakfast.includes(foodItem)) {
+    return `Yes, we're serving ${foodItem.name} today!`;
+  } else {
+    return `Sorry, we aren't serving ${foodItem.name} today.`;
+  }
+}
+
+
+};
 
 module.exports = Chef;
